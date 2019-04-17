4bc334f0b88cfe8a59e815ef417644691a48ae75
Sean McQuillan <seanmcq@google.com>
2019-04-16 22:29:35 -0700

    Code for step 1.

---
+++ motionlayout-start/app/src/main/java/com/google/samples/motionlayoutcodelab/MainActivity.kt
@@ -0,0 +1,41 @@
+/*
+ *   Copyright (C) 2019 The Android Open Source Project
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+package com.google.samples.motionlayoutcodelab
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setupClicks()
+    }
+
+    private fun setupClicks() {
+        findViewById<Button>(R.id.step1).setOnClickListener { navigateTo<Step1Activity>() }
+    }
+
+    private inline fun <reified Destination: Activity> navigateTo() {
+        val intent = Intent(this, Destination::class.java)
+        startActivity(intent)
+    }
+}

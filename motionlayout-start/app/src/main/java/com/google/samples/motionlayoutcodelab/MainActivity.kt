d24eee70bfdd363e32930dc32f2695e5124648d0
Sean McQuillan <seanmcq@google.com>
2019-04-24 03:05:13 -0700

    Finished!

--- motionlayout-start/app/src/main/java/com/google/samples/motionlayoutcodelab/MainActivity.kt
+++ motionlayout-start/app/src/main/java/com/google/samples/motionlayoutcodelab/MainActivity.kt
@@ -17,9 +17,76 @@ package com.google.samples.motionlayoutcodelab
 
 import android.app.Activity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+import kotlin.reflect.KClass
+
+data class Step(
+    val number: String,
+    val name: String,
+    val caption: String,
+    val activity: KClass<out Activity>,
+    val highlight: Boolean = false)
+
+private val data = listOf(
+    Step("Step 1",
+        "Animations with Motion Layout",
+        "Learn how to build a basic animation with Motion Layout. This will crash until you complete the step in the codelab.",
+        Step1Activity::class
+    ),
+    Step("Step 2",
+        "Animating based on drag events",
+        "Learn how to control animations with drag events. This will not display any animation until you complete the step in the codelab.",
+        Step2Activity::class
+    ),
+    Step("Step 3",
+        "Modifying a path",
+        "Learn how to use KeyFrames to modify a path between start and end.",
+        Step3Activity::class
+    ),
+    Step("Step 4",
+        "Building complex paths",
+        "Learn how to use KeyFrames to build complex paths through multiple KeyFrames.",
+        Step4Activity::class
+    ),
+    Step("Step 5",
+        "Changing attributes with motion",
+        "Learn how to resize and rotate views during animations.",
+        Step5Activity::class
+    ),
+    Step("Step 6",
+        "Changing custom attributes",
+        "Learn how to change custom attributes during motion.",
+        Step6Activity::class
+    ),
+    Step("Step 7",
+        "OnSwipe with complex paths",
+        "Learn how to control motion through complex paths with OnSwipe.",
+        Step7Activity::class
+    ),
+    Step("Completed: Steps 2-7",
+        "Steps 2-7 completed",
+        "All changes in steps 2-7 applied",
+        Step7CompletedActivity::class,
+        highlight = true
+    ),
+    Step("Step 8",
+        "Running motion with code",
+        "Learn how to use MotionLayout to build complex collapsing toolbar animations.",
+        Step8Activity::class
+    ),
+    Step("Completed: Step 8 ",
+        "Implements running motion with code",
+        "Changes applied from step 8",
+        Step8CompletedActivity::class,
+        highlight = true
+    )
+)
 
 class MainActivity : AppCompatActivity() {
 
@@ -27,21 +94,47 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        setupClicks()
+        val recyclerView: RecyclerView = findViewById(R.id.recycler_view)
+        recyclerView.adapter = MainAdapter(data)
     }
 
-    private fun setupClicks() {
-        findViewById<Button>(R.id.step1).setOnClickListener { navigateTo<Step1Activity>() }
-        findViewById<Button>(R.id.step2).setOnClickListener { navigateTo<Step2Activity>() }
-        findViewById<Button>(R.id.step3).setOnClickListener { navigateTo<Step3Activity>() }
-        findViewById<Button>(R.id.step4).setOnClickListener { navigateTo<Step4Activity>() }
-        findViewById<Button>(R.id.step5).setOnClickListener { navigateTo<Step5Activity>() }
-        findViewById<Button>(R.id.step6).setOnClickListener { navigateTo<Step6Activity>() }
-        findViewById<Button>(R.id.step7).setOnClickListener { navigateTo<Step7Activity>() }
+}
+
+class MainAdapter(val data: List<Step>) : RecyclerView.Adapter<MainViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
+        return MainViewHolder(view as CardView)
     }
 
-    private inline fun <reified Destination: Activity> navigateTo() {
-        val intent = Intent(this, Destination::class.java)
-        startActivity(intent)
+    override fun getItemCount() = data.size
+
+    override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
+        holder.bind(data[position])
     }
+
+}
+
+class MainViewHolder(val cardView: CardView) : RecyclerView.ViewHolder(cardView) {
+    val header: TextView = cardView.findViewById(R.id.header)
+    val description: TextView = cardView.findViewById(R.id.description)
+    val caption: TextView = cardView.findViewById(R.id.caption)
+
+    fun bind(step: Step) {
+        header.text = step.number
+        description.text = step.name
+        caption.text = step.caption
+        val context = cardView.context
+        cardView.setOnClickListener {
+            val intent = Intent(context, step.activity.java)
+            context.startActivity(intent)
+        }
+        val color = if (step.highlight) {
+            context.resources.getColor(R.color.secondaryLightColor)
+        } else {
+            context.resources.getColor(R.color.primaryTextColor)
+        }
+        header.setTextColor(color)
+        description.setTextColor(color)
+    }
+
 }
